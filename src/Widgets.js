5a703f12e1271cc8f63fcd00a792a6da9bd1aec3
Brian Sahota <bpsahota@gmail.com>
2021-02-01 21:17:15 -0500

    working on widget

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import './Widgets.css'
+import SearchIcon from '@material-ui/icons/Search';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from 'react-twitter-embed'
 
 const Widgets = () => {
 return(
     <div className="widgets" >
-        <h2>Widgets heaven</h2>
+        <div className="widgets__input" >
+            <SearchIcon className="widgets_searchIcon" />
+            <input placeholder="suck it" type="text" />
+        </div>
+
+        <div className="widgets__widgetContainer" >
+            <h2>What's happenin</h2>
+
+            <TwitterTweetEmbed tweetId={"858551177860055040"} />
+
+            <TwitterTimelineEmbed 
+            sourceType="profile"
+            screenName="WSBgod"
+            options={{ height: 400 }}
+            />
+
+            <TwitterShareButton
+            url={"pornhub.com"}
+            options={{ text: "i luv pinot", via: "scratchytube" }}
+            />
+
+
+        </div>
+
+
     </div>
 )
 }
